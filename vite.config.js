21a36d9ac032f29353c74e34b6d002c65ca0afb9
Farahhamdy2 <FARAHHAMDY567@GMAIL.COM>
2025-06-08 01:55:19 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Herafy/',
+  base: process.env.NODE_ENV === 'production' ? '/Herafy/' : '/',
   plugins: [react()],
 })
